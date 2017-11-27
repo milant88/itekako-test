5d35df4a6722d46d964afea6b1a52103895c0cec
milant88 <milan.tomic@computerrock.com>
2017-11-27 20:22:59 +0100

    render and style of list items

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,18 +12,20 @@ class Home extends Component {
 
     render() {
         return (
-            <div className="row no-gutters">
-                {
-                    this.props.office.map(office =>
-                        <OfficeItem
-                            key={office.id}
-                            name={office.name}
-                            description={office.description}
-                            image={office.photo}
-                        />
-                    )
-                }
-                {console.log(this.props.office)}
+            <div className="container">
+                <div className="row office-list">
+                    {
+                        this.props.office.map(office =>
+                            <OfficeItem
+                                key={office.id}
+                                name={office.name}
+                                description={office.description}
+                                photo={office.photo}
+                            />
+                        )
+                    }
+                    {console.log(this.props.office)}
+                </div>
             </div>
         )
     }
--- src/components/parts/OfficeItem.js
+++ src/components/parts/OfficeItem.js
@@ -4,10 +4,12 @@ class OfficeItem extends Component {
 
     render() {
         return (
-            <div className="office-item">
-                <img src={this.props.photo} alt='*'/>
-                <span>{this.props.name}</span>
-                <span>{this.props.description}</span>
+            <div className="item">
+                {this.props.photo ? <img src={this.props.photo} alt='*'/> : <div className="item__placeholder">{this.props.name[0]}</div>}
+                <div className="item__wrapper">
+                    <div className="item__name">{this.props.name}</div>
+                    <div className="item__description">{this.props.description}</div>
+                </div>
             </div>
         )
     }
